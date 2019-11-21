9dbe1905ac11ea5e3721d9f2f25b4b933b16b545
Tomasz Wazny <wazny.tomek@gmail.com>
2019-11-21 21:07:12 +0100

    basic db setup

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3');
+
 
 let app = express();
-
-app.use(express.static(path.join(__dirname, '/client')));
-
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 
-var server = app.listen(PORT,() => {
-    console.log(`Server listening onc as port: ${PORT}.`);
+app.use(express.static(path.join(__dirname, '/client')));
+
+let database = new sqlite3.Database('./db/DATABASE.sqlite',(err) => {
+    if (err) {
+      console.error(err.message);
+    }
+
+    
+    var server = app.listen(PORT,() => {
+        console.log(`Server listening onc as port: ${PORT}.`);
+    });
+
 });
+
+
+
